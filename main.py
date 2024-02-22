5a0b73299590e477656320b73e9028641dfabf6a
sk01c <160864725+sk01c@users.noreply.github.com>
2024-02-22 16:40:45 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import turtle
+turd = turtle.Turtle()
+turd.color("DarkGreen")
+turd.shape("turtle")
+turd.forward(100)
+
+my_screen = turtle.Screen()
+(my_screen.canvheight)
+my_screen.exitonclick()
